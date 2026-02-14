bb0e08c70059e58279c27709518e02e8d791ff6f
kanishk1602 <aaaaa.bbbb.ccc.dd.e.dd.ccc.bbbb.aaaaa@gmail.com>
2026-02-14 22:06:27 +0530

    minor fix

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
 
 app.get("/", (req,res)=>{
     res.send("CI/CD Working - Change 2.0");
